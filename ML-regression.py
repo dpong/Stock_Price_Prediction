c93ef07b4847788558e581918b801a3456ae2692
dpong <powenperng@gmail.com>
2019-07-16 12:12:34 +0800

    1.些許調整程式結構。2.股價預測結果可以和歷史資料疊圖對照。

--- ML-regression.py
+++ ML-regression.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Jul  9 15:34:44 2019
+Inspired by sentdex
 
 @author: dpong
 """
@@ -9,23 +9,27 @@ Created on Tue Jul  9 15:34:44 2019
 import pandas as pd
 import matplotlib.pyplot as plt
 import quandl, datetime
-import math, pickle, talib
+import math, pickle
 import numpy as np
-from sklearn import preprocessing, neighbors
+from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from matplotlib import style
-from sklearn.ensemble import VotingClassifier
 from talib import abstract
 
 style.use('ggplot')
 
+
+def get_data(ticker):
+    df = quandl.get('WIKI/'+ticker)
+    df = df[['Adj. Open','Adj. High','Adj. Low','Adj. Close','Adj. Volume']]
+    with open('{}_data_from_quandl.pickle'.format(ticker),'wb') as f:
+        pickle.dump(df,f)
+
 def ML_training(ticker):
     
-
-    df = quandl.get('WIKI/'+ticker)
-
-    df = df[['Adj. Open','Adj. High','Adj. Low','Adj. Close','Adj. Volume']]
+    pickle_in = open('{}_data_from_quandl.pickle'.format(ticker),'rb') 
+    df = pickle.load(pickle_in)
     
     df['HL_PCT'] = (df['Adj. High']-df['Adj. Low'])/df['Adj. Close'] * 100
     df['PCT_change'] = (df['Adj. Close']-df['Adj. Open'])/df['Adj. Open'] * 100
@@ -38,25 +42,28 @@ def ML_training(ticker):
     #df['SlowK'] = STOCH['slowk']
     #df['SlowD'] = STOCH['slowd']
     df['RSI'] = abstract.RSI(df) #算出RSI
-    
 
-    df = df[['close','HL_PCT','PCT_change','RSI','Adj. Volume']]
-
-    forecast_col = 'close'
+    df = df[['close','HL_PCT','PCT_change','Adj. Volume','RSI']]
 
     df.fillna(-99999,inplace=True) #pandas 把沒資料的都改成-99999
 
-    forecast_out = 30 #int(math.ceil(0.005*len(df))) #4捨5入
+    forecast_out = int(math.ceil(0.005*len(df))) #4捨5入
 
-    df['label'] = df[forecast_col].shift(-forecast_out)
-
-    X = np.array(df.drop(['label'],axis=1))
+    df['label'] = df['close'].shift(-forecast_out)
+    
+    df_preprocess = df[['close','HL_PCT','PCT_change','Adj. Volume','RSI']]
+    #把close納入的預測準確度會大幅提升
+    
+    X = np.array(df_preprocess)
     X = preprocessing.scale(X)
+    
     X = X[:-forecast_out]
     X_lately = X[-forecast_out:]
-
+    
+    df_origin = df
+    df_origin = df_origin['close'] #之後畫圖的對照
+    
     df.dropna(inplace=True)
-
     y = np.array(df['label'])
 
     X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2)
@@ -68,16 +75,15 @@ def ML_training(ticker):
 
     print(accuracy)
     
-    pickle_save = [clf,df,X_lately]
+    pickle_save = [clf,df,X_lately,forecast_out,df_origin]
     
     with open('{}_LinearRegression.pickle'.format(ticker),'wb') as f:
-        pickle.dump(pickle_save,f)
+        pickle.dump(pickle_save,f)   
 
 
-
-def prediction(ticker,days=40):
+def prediction(ticker,days=100):
     pickle_in = open('{}_LinearRegression.pickle'.format(ticker),'rb') 
-    clf2,df,X_lately = pickle.load(pickle_in) 
+    clf2,df,X_lately,forecast_out,df_origin = pickle.load(pickle_in) 
 
     
     forecast_set = clf2.predict(X_lately)
@@ -93,20 +99,25 @@ def prediction(ticker,days=40):
         next_date = datetime.datetime.fromtimestamp(next_unix)
         next_unix += one_day
         df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)]+[i]
-
-    df = df[-days:].drop(columns=['HL_PCT','PCT_change','label'])
+    
+    
+    df = df.drop(columns=['HL_PCT','PCT_change','label','RSI'])
+  
+    df_origin=df_origin[:-6] #調整數據
+    df = df[-days:]
+    df_origin=df_origin[-days+20:]
     
     df['Combined_Close'] = df['close']
-    for i in range(1,11):
+    for i in range(1,forecast_out+1):
         df['Combined_Close'].iloc[-i] = df['Forecast_Close'].iloc[-i]
     
-    df['close'].plot()
+    df_origin.plot()
     df['Forecast_Close'].plot()
     plt.legend(loc=4)
     plt.xlabel('Date')
     plt.ylabel('Price')
     plt.title('{}'.format(ticker))
-    return df
+    
 
 
 
