d12961a2a45a7ef01bcafa24d033d1f7a9425a40
dpong <powenperng@gmail.com>
2019-08-07 20:49:01 +0800

    提升了程式碼的效率。

--- ML-regression.py
+++ ML-regression.py
@@ -7,121 +7,98 @@ Inspired by sentdex
 """
 
 import pandas as pd
+import pandas_datareader as pdr
 import matplotlib.pyplot as plt
-import quandl, datetime
-import math, pickle
 import numpy as np
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from matplotlib import style
-from talib import abstract
+from datetime import datetime, date, timedelta
 
 style.use('ggplot')
 
-
-def get_data(ticker):
-    df = quandl.get('WIKI/'+ticker)
-    df = df[['Adj. Open','Adj. High','Adj. Low','Adj. Close','Adj. Volume']]
-    with open('{}_data_from_quandl.pickle'.format(ticker),'wb') as f:
-        pickle.dump(df,f)
-
-def ML_training(ticker):
+class Stock_price_prediction():
     
-    pickle_in = open('{}_data_from_quandl.pickle'.format(ticker),'rb') 
-    df = pickle.load(pickle_in)
-    
-    df['HL_PCT'] = (df['Adj. High']-df['Adj. Low'])/df['Adj. Close'] * 100
-    df['PCT_change'] = (df['Adj. Close']-df['Adj. Open'])/df['Adj. Open'] * 100
-    #用talib一定要rename一下key才會對應的到
-    df.rename(columns = {'Adj. Open':'open',
-                         'Adj. High':'high',
-                         'Adj. Low':'low',
-                         'Adj. Close':'close'}, inplace = True)
-    #STOCH = abstract.STOCH(df)  #算出KD
-    #df['SlowK'] = STOCH['slowk']
-    #df['SlowD'] = STOCH['slowd']
-    df['RSI'] = abstract.RSI(df) #算出RSI
+    def __init__ (self):
+        self.df = pd.DataFrame()
+        self.df_origin = pd.DataFrame()
+        self.train_accuracy = None
+        self.clf = LinearRegression()
+        self.predict_date_start = None
+        self.predict_length = None
+        self.predict_df = pd.DataFrame()
+        
+    def choose_model(self):
+        pass
+        
+        
+    def ML_training(self,ticker,forecast_out=15):
+        self.predict_length = forecast_out #提供查詢
+        self.ticker = ticker
+        self.df_origin = pdr.DataReader(self.ticker,'yahoo')
+        
+        
+        #用talib一定要rename一下key才會對應的到
+        self.df_origin.rename(columns = {'Open':'open',
+                                  'High':'high',
+                                  'Low':'low',
+                                  'Close':'close'}, inplace = True)
+        self.df_origin.drop(columns=['Adj Close'],inplace=True)
+        self.df_origin['HL_PCT'] = (self.df_origin['high']-self.df_origin['low'])/self.df_origin['close'] * 100
+        self.df_origin['PCT_change'] = (self.df_origin['close']-self.df_origin['open'])/self.df_origin['open'] * 100
+        
+        self.df = self.df_origin[:-forecast_out].copy()    #避免pandas一直跳警告
+        
+        self.predict_date_start = datetime.strptime(str(self.df.iloc[-self.predict_length].name), "%Y-%m-%d %H:%M:%S")
+        self.predict_date_start = datetime.date(self.predict_date_start)
+        test_df = self.df.dropna(how='any')
+        test_df['label'] = self.df['close'].shift(-self.predict_length)
+        df_preprocess = test_df.drop(columns=['label'])    
+        X = np.array(df_preprocess)
+        #X = preprocessing.scale(X)        
+        X = X[:-self.predict_length] 
+        y = np.array(test_df['label'].dropna(how='any'))  
+        X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2)
+        self.clf.fit(X_train,y_train)  
+        self.train_accuracy = self.clf.score(X_test,y_test)            
+        
+    def prediction_test(self,show_days=20):
+        
+        self.predict_df = self.df[-self.predict_length * 2:]
+        X = np.array(self.predict_df[:-self.predict_length])
+        
+        #X = preprocessing.scale(X)
+        y = self.clf.predict(X)
+        #資料整理
+        pre_y = np.full(self.predict_length,np.nan)
+        y = np.append(pre_y,y)
+        self.predict_df['prediction'] = y
+        self.df['prediction'] = np.nan
+        self.df = self.df[:-self.predict_length-1]
+        self.df = pd.concat([self.df,self.predict_df],axis=0)
+        #畫圖
+        self.df['close'][-show_days:].plot()
+        self.df['prediction'][-show_days:].plot()
+        plt.legend(loc=4)
+        plt.xlabel('Date')
+        plt.ylabel('Price')
+        plt.title('{}'.format(self.ticker))
+        
+    def predict_future(self):
+        self.predict_df = self.df_origin[-self.predict_length:]
+        X = np.array(self.predict_df)
+        y = self.clf.predict(X)
+        print('T: '+str(self.predict_df['close'][-1]))
+        for i in range(y.shape[0]):
+            print('T+{}: '.format(i+1)+ str(round(y[i],2)))
+        
 
-    df = df[['close','HL_PCT','PCT_change','Adj. Volume','RSI']]
-
-    df.fillna(-99999,inplace=True) #pandas 把沒資料的都改成-99999
-
-    forecast_out = int(math.ceil(0.005*len(df))) #4捨5入
-
-    df['label'] = df['close'].shift(-forecast_out)
+if __name__=='__main__':
+    s = Stock_price_prediction()
+    s.ML_training('^TWII',forecast_out=10)
+    #s.prediction()
+    s.predict_future()
     
-    df_preprocess = df[['close','HL_PCT','PCT_change','Adj. Volume','RSI']]
-    #把close納入的預測準確度會大幅提升
-    
-    X = np.array(df_preprocess)
-    X = preprocessing.scale(X)
-    
-    X = X[:-forecast_out]
-    X_lately = X[-forecast_out:]
-    
-    df_origin = df
-    df_origin = df_origin['close'] #之後畫圖的對照
-    
-    df.dropna(inplace=True)
-    y = np.array(df['label'])
-
-    X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2)
-    clf = LinearRegression()
-                       
-    clf.fit(X_train,y_train)
-    
-    accuracy = clf.score(X_test,y_test)
-
-    print(accuracy)
-    
-    pickle_save = [clf,df,X_lately,forecast_out,df_origin]
-    
-    with open('{}_LinearRegression.pickle'.format(ticker),'wb') as f:
-        pickle.dump(pickle_save,f)   
-
-
-def prediction(ticker,days=100):
-    pickle_in = open('{}_LinearRegression.pickle'.format(ticker),'rb') 
-    clf2,df,X_lately,forecast_out,df_origin = pickle.load(pickle_in) 
-
-    
-    forecast_set = clf2.predict(X_lately)
-    
-    df['Forecast_Close'] = np.nan
-
-    last_date = df.iloc[-1].name
-    last_unix = last_date.timestamp()
-    one_day = 86400
-    next_unix = last_unix + one_day
-
-    for i in forecast_set:
-        next_date = datetime.datetime.fromtimestamp(next_unix)
-        next_unix += one_day
-        df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)]+[i]
-    
-    
-    df = df.drop(columns=['HL_PCT','PCT_change','label','RSI'])
-  
-    df_origin=df_origin[:-6] #調整數據
-    df = df[-days:]
-    df_origin=df_origin[-days+20:]
-    
-    df['Combined_Close'] = df['close']
-    for i in range(1,forecast_out+1):
-        df['Combined_Close'].iloc[-i] = df['Forecast_Close'].iloc[-i]
-    
-    df_origin.plot()
-    df['Forecast_Close'].plot()
-    plt.legend(loc=4)
-    plt.xlabel('Date')
-    plt.ylabel('Price')
-    plt.title('{}'.format(ticker))
-    
-
-
-
-
-
 
 
